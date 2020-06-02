8acee616d80475cf30a62cb0f333d5e6d4f77181
Fernando Bahamondes <ferbaco@gmail.com>
2020-06-02 15:23:53 -0400

    Fix Github and Twitter usernames in Readme. Fix remaining rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true, length: { maximum: 16, too_long: '%{count} characters is the maximum allowed' }
-  validates :password, presence: true, length: { minimum: 8, too_short: '%{count} characters is the minimum allowed' }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 16,
+                                                               too_long: '%<count>s characters is the maximum allowed' }
+  validates :password, presence: true, length: { minimum: 8, too_short: '%<count>s characters is the minimum allowed' }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   has_many :posts
   has_many :comments
