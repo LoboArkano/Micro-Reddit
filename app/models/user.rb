bd78b8313732174b661711dc2532d925e79a588e
Fernando Bahamondes <ferbaco@gmail.com>
2020-06-02 12:23:33 -0400

    Add length validations to User name and password. Add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: {maximum: 16, too_long: "%{count} characters is the maximum allowed" } 
+  validates :password, presence: true, length: {minimum: 8, too_short: "%{count} characters is the minimum allowed"}
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
